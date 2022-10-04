24becf51fe5449e031e128edaa3a64ea4b7bd475
Asadchudhary <108215874+Asadchudhary@users.noreply.github.com>
2022-10-04 17:26:54 +0500

    GithubUsers
    A list of github users.

---
+++ src/useEffect/Card.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <>
+      <div className="col-10 col-md-4 mt-5 mb-2">
+        <div className="card p-2">
+          <div className="d-flex align-items-center">
+            <div className="image">
+              <img src={props.img} className="rounded imgsize" alt="" />
+            </div>
+            <div className="ml-3 w-100">
+              <h4 className="mb-0 mt-0 text-left">{props.login}</h4>
+              <span className="text-left">Developer</span>
+              <div className="p-2 mt-2 d-flex justify-content-between rounded text-white  stats">
+                <div className="d-flex flex-column bgc">
+                  <span className="articles">Article</span>
+                  <span className="number1">38</span>
+                </div>
+                <div className="d-flex flex-column bgc">
+                  <span className="followers">Followers</span>
+                  <span className="number2">38</span>
+                </div>
+                <div className="d-flex flex-column bgc">
+                  <span className="ratings">Rating</span>
+                  <span className="number3">38</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
---
+++ src/useEffect/UseEffectApi.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import Loading from "./Loading";
+
+const UseEffectApi = () => {
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const token = "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";
+    try {
+      setLoading(false);
+      const response = await fetch("https://api.github.com/users", {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      });
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.log("My Error is :" + error);
+    }
+  };
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  {
+    if (loading) {
+      return <Loading />;
+    }
+  }
+  return (
+    <>
+      <div className="body">
+        <h1 className="text-center pt-3 headingStyle">List of Github Users</h1>
+        <div className="container-fluid mt-1">
+          <div className="row text-center">
+            {users.map((val, index) => {
+              return (
+                <Card key={index} img={val.avatar_url} login={val.login} />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UseEffectApi;
